4b8cbaea1169869986a2ffac0269877d22fee40c
Ere-0 <Okere.joshua@yahoo.com>
2022-10-24 13:33:09 +0100

    model for my first django app

--- songcrud/musicapp/apps.py
+++ songcrud/musicapp/apps.py
@@ -1,6 +1,7 @@
+from email.policy import default
 from django.apps import AppConfig
 
 
 class MusicappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-     name = 'musicapp'
+    name = 'musicapp'
